b6e32523b32bdc8b127692bf65bc54c3ba19c2b6
Ben Grabow <bgrabow@splashfinancial.com>
2025-12-16 17:01:49 -0600

    Solve d01p1

--- src/day_01.rs
+++ src/day_01.rs
@@ -29,3 +29,38 @@ fn rotations(input: &str) -> Vec<Rotation> {
 pub fn parse_input() -> Vec<Rotation> {
     rotations(&util::file_to_string("resources/input_01.txt"))
 }
+
+fn cycle(value: i32, min: i32, max: i32) -> i32 {
+    let range_size = max - min + 1;
+    let mut adjusted_value = value - min;
+    adjusted_value = adjusted_value % range_size;
+    if adjusted_value < 0 {
+        adjusted_value += range_size;
+    }
+    adjusted_value + min
+}
+
+pub fn solve_part1() -> String {
+    let dial_start = 50;
+    let dial_max = 99;
+    let dial_min = 0;
+
+    let password_num = parse_input().iter()
+        .map(|rot| {
+        match rot.direction {
+            Direction::Left => {
+                -rot.distance
+            },
+            Direction::Right => {
+                rot.distance
+            },
+        }
+    }).fold(vec![dial_start], |acc, x| {
+        let last = *acc.last().unwrap();
+        let mut new_acc = acc;
+        new_acc.push(cycle(last + x, dial_min, dial_max));
+        new_acc
+    }).iter().filter(|&x| x == &0).count();
+
+    password_num.to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day_01;
 mod util;
 
+#[allow(dead_code)]
 fn display_rotation(rotation: &day_01::Rotation) -> String {
     let dir = match rotation.direction {
         day_01::Direction::Left => "L",
@@ -12,8 +13,13 @@ fn display_rotation(rotation: &day_01::Rotation) -> String {
 }
 
 fn main() {
-    let result = day_01::parse_input();
-    println!("Result: {}", result.iter()
-        .map(|rot| display_rotation(rot))
-        .collect::<Vec<String>>().join("\n"));
+    use std::time::Instant;
+    let now = Instant::now();
+
+    // let result = day_01::parse_input();
+    // println!("Result: {}", result.iter()
+    //     .map(|rot| display_rotation(rot))
+    //     .collect::<Vec<String>>().join("\n"));
+    println!("Day 01 solution\npart 1: {}\npart 2: {}", day_01::solve_part1(), "NYI");
+    println!("Time: {:?}", now.elapsed());
 }
