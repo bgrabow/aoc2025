26ec935cf8e96fb7916a36bc585041ed7c02adc8
Ben Grabow <ben.grabow@gmail.com>
2026-01-21 12:24:01 -0600

    Add RCF testing and solve d1p2

--- clojure/src/day_01.clj
+++ clojure/src/day_01.clj
@@ -1,5 +1,11 @@
 (ns day-01
-  (:require [clojure.string :as str]))
+  (:require
+    [clojure.string :as str]
+    [hyperfiddle.rcf :refer [tests]]))
+
+(def DIAL_START 50)
+(def DIAL_MAX 99)
+(def DIAL_RANGE (inc DIAL_MAX))
 
 (defn parse-line
   [line]
@@ -12,12 +18,23 @@
   [s]
   (map parse-line (str/split-lines (str/trim s))))
 
-(defn zeroes-between
-  [x y])
+(defn div-euclid
+  [x y]
+  (long (Math/floor (/ x y))))
 
-(def DIAL_START 50)
-(def DIAL_MAX 99)
-(def DIAL_MIN 0)
+(tests
+  (div-euclid -1 DIAL_RANGE) := -1
+  (div-euclid 100 DIAL_RANGE) := 1
+  (div-euclid 99 DIAL_RANGE) := 0
+  (div-euclid -1 DIAL_RANGE) := -1
+  (div-euclid -1 DIAL_RANGE) := -1
+  (div-euclid -1 DIAL_RANGE) := -1)
+
+(defn zeroes-between
+  [from to]
+  (if (<= from to)
+    (- (div-euclid to DIAL_RANGE) (div-euclid from DIAL_RANGE))
+    (zeroes-between (- from) (- to))))
 
 (defn normalize-dial
   [x]
@@ -29,19 +46,19 @@
     (reductions + DIAL_START)
     (map normalize-dial)
     (filter zero?)
-    (count))
-  )
+    (count)))
 
 (defn solve-part-2
   [input]
   (->> (rotations input)
-            (partition 2 1)
-            (map #(apply zeroes-between %))
-            ;(reduce +)
-    ))
+    (reductions + DIAL_START)
+    (partition 2 1)
+    (map #(apply zeroes-between %))
+    (reduce +)))
 
 (comment
   (let [input (slurp "../rust/resources/input_01.txt")
         example (slurp "../rust/resources/input_01_example.txt")]
     #_(solve-part-1 input)
-    (solve-part-2 example)))
+    ;(solve-part-2 example)
+    (solve-part-2 input)))
