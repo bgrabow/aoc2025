5c70d1cfacb5c3320d1ffd48fce0971082d5d158
Ben Grabow <ben.grabow@gmail.com>
2026-01-22 09:36:24 -0600

    Solve d5p2

--- clojure/src/day_05.clj
+++ clojure/src/day_05.clj
@@ -37,12 +37,33 @@
   (let [{:keys [fresh-ranges ingredients]} (parse s)]
     (count (filter (partial fresh? fresh-ranges) ingredients))))
 
+(defn solve-part-2
+  [s]
+  (let [{:keys [fresh-ranges]} (parse s)
+        {:keys [frontier groups]} (reduce
+                                    (fn [acc r]
+                                      (let [[lb ub] (:frontier acc)
+                                            [lb' ub'] r]
+                                        (cond
+                                          (not (:frontier acc)) (assoc acc :frontier r)
+                                          (<= lb lb' ub) (assoc acc :frontier [(min lb lb') (max ub ub')])
+                                          :else (-> acc
+                                                  (update :groups conj [lb ub])
+                                                  (assoc :frontier r)))))
+                                    {:frontier nil :groups []}
+                                    (sort-by (juxt first second) fresh-ranges))]
+    (->> (conj groups frontier)
+      (map #(inc (- (apply - %))))
+      (reduce +))))
+
 (tests
-  (solve-part-1 (slurp "../rust/resources/input_05_example.txt")) := 3)
+  (solve-part-1 (slurp "../rust/resources/input_05_example.txt")) := 3
+  (solve-part-2 (slurp "../rust/resources/input_05_example.txt")) := 14)
 
 (comment
   (let [input (slurp "../rust/resources/input_05.txt")
         example (slurp "../rust/resources/input_05_example.txt")]
-    (solve-part-1 input)))
+    (solve-part-1 input)
+    (solve-part-2 input)))
 
 
