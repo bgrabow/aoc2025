93a60fa137e9f1b800723e58415ba544c72fdd57
Ben Grabow <ben.grabow@gmail.com>
2026-01-22 16:49:30 -0600

    Solve d8p2

--- clojure/src/day_08.clj
+++ clojure/src/day_08.clj
@@ -2,15 +2,14 @@
   (:require
     [clojure.set :as set]
     [clojure.string :as str]
-    [hyperfiddle.rcf :refer [tests]]
-    [clojure.math.numeric-tower :as math]))
+    [hyperfiddle.rcf :refer [tests]]))
 
 (defn parse
   [s]
   (mapv #(mapv parse-long (re-seq #"\d+" %)) (str/split-lines s)))
 
 (defn solve-part-1
-  [s]
+  [n s]
   (let [boxes (parse s)
         adjacency-queue (vec
                           (dedupe
@@ -20,29 +19,82 @@
                                     :when (not= a b)]
                                 (let [distance-squared (reduce + (mapv (comp #(* % %) -) a b))]
                                   [distance-squared (vec (sort [a b]))])))))]
-    (reductions
-      (fn [circuits next-candidate]
-        (let [[distance] next-candidate
-              [a b] (seq (second next-candidate))
-              a-group (first (filter #(contains? % a) circuits))
-              b-group (first (filter #(contains? % b) circuits))]
-          (if (and a-group b-group
-                (= a-group b-group))
-            circuits
-            (conj (disj circuits a-group b-group)
-              (set/intersection a-group b-group)))))
-      #{}
-      adjacency-queue)))
+    (->> (reductions
+           (fn [circuits next-candidate]
+             (let [[_distance] next-candidate
+                   [a b] (seq (second next-candidate))
+                   a-circuit (or (first
+                                   (filter
+                                     #(contains? % a)
+                                     circuits))
+                               #{a})
+                   b-circuit (or (first
+                                   (filter
+                                     #(contains? % b)
+                                     circuits))
+                               #{b})]
+               (if (= a-circuit b-circuit)
+                 circuits
+                 (conj (disj circuits a-circuit b-circuit)
+                   (set/union a-circuit b-circuit)))))
+           #{}
+           (take n adjacency-queue))
+      (last)
+      (sort-by #(- (count %)))
+      (take 3)
+      (map count)
+      (reduce *))))
 
-(let [[a b] (seq #{1 2})]
-  [a b])
+(tests
+  (solve-part-1 10 (slurp "../rust/resources/input_08_example.txt")) := 40)
 
 (defn solve-part-2
-  [s])
+  [s]
+  (let [boxes (parse s)
+        target-circuit-size (count boxes)
+        adjacency-queue (vec
+                          (dedupe
+                            (sort-by first
+                              (for [a boxes
+                                    b boxes
+                                    :when (not= a b)]
+                                (let [distance-squared (reduce + (mapv (comp #(* % %) -) a b))]
+                                  [distance-squared (vec (sort [a b]))])))))
+        [_circuits [a b]] (->> (map vector
+                                (drop 1
+                                  (reductions
+                                    (fn [circuits next-candidate]
+                                      (let [[_distance] next-candidate
+                                            [a b] (seq (second next-candidate))
+                                            a-circuit (or (first
+                                                            (filter
+                                                              #(contains? % a)
+                                                              circuits))
+                                                        #{a})
+                                            b-circuit (or (first
+                                                            (filter
+                                                              #(contains? % b)
+                                                              circuits))
+                                                        #{b})]
+                                        (if (= a-circuit b-circuit)
+                                          circuits
+                                          (conj (disj circuits a-circuit b-circuit)
+                                            (set/union a-circuit b-circuit)))))
+                                    #{}
+                                    adjacency-queue))
+                                (map second adjacency-queue))
+                           (filter (fn [[circuits]]
+                                     (and (= 1 (count circuits))
+                                       (= target-circuit-size (count (first circuits))))))
+                           (first))]
+    (* (first a) (first b))))
+
+(tests
+  (solve-part-2 (slurp "../rust/resources/input_08_example.txt")) := 25272)
 
 (comment
   (parse (slurp "../rust/resources/input_08_example.txt"))
-  (solve-part-1 (slurp "../rust/resources/input_08.txt"))
-  (solve-part-1 (slurp "../rust/resources/input_08_example.txt"))
+  (solve-part-1 1000 (slurp "../rust/resources/input_08.txt"))
+  (solve-part-1 10 (slurp "../rust/resources/input_08_example.txt"))
   (solve-part-2 (slurp "../rust/resources/input_08.txt"))
   (solve-part-2 (slurp "../rust/resources/input_08_example.txt")))
