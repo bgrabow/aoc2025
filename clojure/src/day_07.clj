ad3b504d8c6ab6c542e80bb801e99e0fe7fe756d
Ben Grabow <ben.grabow@gmail.com>
2026-01-22 13:18:03 -0600

    WIP: this doesn't work. Need to go forwards instead of backwards

--- clojure/src/day_07.clj
+++ clojure/src/day_07.clj
@@ -43,7 +43,28 @@
   (solve-part-1 (slurp "../rust/resources/input_07_example.txt")) := 21)
 
 (defn solve-part-2
-  [s])
+  [s]
+  (let [{:keys [beams splitters]} (parse s)
+        beam-paths (reductions split beams splitters)]
+    (reduce
+      (fn [timelines [beams splitters]]
+        (let [splits (set/intersection beams splitters)]
+          (merge (apply dissoc timelines (mapcat (juxt inc dec) splits))
+            (zipmap splits (map (fn [x]
+                                  (try (+ (get timelines (dec x))
+                                     (get timelines (inc x)))
+                                       (catch Exception _
+                                         (throw (ex-info "Timeline not found" {:x x
+                                                                               :dec-x (type (dec x))
+                                                                               :inc-x (type (inc x))
+                                                                               :timelines timelines
+                                                                               :beams beams
+                                                                               :splitters splitters})))))
+                             splits)))))
+      (zipmap (last beam-paths) (repeat 1))
+      (map vector
+        (drop 1 (reverse beam-paths))
+        (reverse splitters)))))
 
 (comment
   (solve-part-1 (slurp "../rust/resources/input_07.txt"))
