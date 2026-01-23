98d40c548a17514048395c211026aaf9da0098ce
Ben Grabow <ben.grabow@gmail.com>
2026-01-22 21:53:55 -0600

    Solve d10p1

---
+++ clojure/src/day_10.clj
@@ -0,0 +1,50 @@
+(ns day-10
+  (:require
+    [clojure.string :as str]
+    [clojure.math.combinatorics :as combo]
+    [hyperfiddle.rcf :refer [tests]]))
+
+(defn machine
+  [s]
+  (let [target-lights (mapv {\. 0 \# 1} (second (re-find #"\[([\.#]+)\]" s)))
+        button-schematics (mapv #(mapv parse-long (re-seq #"\d+" %))
+                            (re-seq #"\(\d+(?:,\d+)*\)" s))
+        joltage-requirements (mapv parse-long (re-seq #"\d+" (re-find #"\{\d+(?:,\d+)*\}" s)))]
+    {:target-lights        target-lights
+     :initial-lights       (vec (repeat (count target-lights) 0))
+     :button-schematics    button-schematics
+     :button-combinations  (sort-by count (remove empty? (combo/subsets button-schematics)))
+     :joltage-requirements joltage-requirements
+     :joltage-levels       (vec (repeat (count joltage-requirements) 0))}))
+
+(defn parse
+  [s]
+  (map machine (str/split-lines s)))
+
+(defn press-button
+  [lights button]
+  (reduce #(update %1 %2 {0 1 1 0}) lights button))
+
+(defn solve-part-1
+  [s]
+  (let [machines (parse s)]
+    (reduce +
+      (for [{:keys [button-combinations target-lights initial-lights]} machines]
+        (count (first
+                 (filter (fn [buttons]
+                           (= target-lights (reduce press-button initial-lights buttons)))
+                   button-combinations)))))))
+
+(tests
+  (solve-part-1 (slurp "../rust/resources/input_10_example.txt")) := 7)
+
+(defn solve-part-2
+  [_s])
+
+(comment
+  (parse (slurp "../rust/resources/input_10_example.txt"))
+  (parse (slurp "../rust/resources/input_10.txt"))
+  (solve-part-1 (slurp "../rust/resources/input_10.txt"))
+  (solve-part-1 (slurp "../rust/resources/input_10_example.txt"))
+  (solve-part-2 (slurp "../rust/resources/input_10.txt"))
+  (solve-part-2 (slurp "../rust/resources/input_10_example.txt")))
